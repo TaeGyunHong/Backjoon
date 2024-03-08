eeece256fabc4009061fcd91b91217f026668653
TaeGyunHong <138078447+TaeGyunHong@users.noreply.github.com>
2024-03-08 23:22:30 +0900

    Create 2475.cpp

---
+++ 2475.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, square = 0;
+
+    for (int i = 0; i < 5; i++) {
+        cin >> num;
+        square += num * num;
+    }
+    cout << square % 10;
+}
